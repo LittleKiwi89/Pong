d39b26553bf34318ffdb8acf3da3186faa7affd3
LittleKiwi89 <NicoleMcIntosh@myop.ac.nz>
2021-12-14 12:35:55 +1300

    updated pong

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,9 +1,18 @@
+import javax.swing.JFrame;
 
-public class Pong {
+public class Pong extends JFrame {
 
+	public Pong() {
+		setTitle("Pong");
+		setSize(100, 200);
+		setResizable(true);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		new Pong();
 	}
 
 }
