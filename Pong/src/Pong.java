cf9445d0adf421088bfa40b5845ff0dbdfcb730e
LittleKiwi89 <NicoleMcIntosh@myop.ac.nz>
2021-12-14 18:57:42 +1300

    Sprite all done

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,17 +1,13 @@
-import java.awt.Color;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+
 
 
 public class Pong extends JFrame {
 		private final static String WINDOW_TITLE = "Pong";
 		private final static int WINDOW_WIDTH = 800;
 		private final static int WINDOW_HEIGHT = 600;
-		private final static Color BACKGROUND_COLOUR = Color.BLACK;
+		
 		
 	public Pong() {
 		setTitle(WINDOW_TITLE);
@@ -22,44 +18,24 @@ public class Pong extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	
-	public class PongPanel extends JPanel implements ActionListener, KeyListener {
-		
-		public PongPanel() {
-			setBackground(BACKGROUND_COLOUR);
-		}
 
-		@Override
-		public void keyTyped(KeyEvent event) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void keyPressed(KeyEvent event) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void keyReleased(KeyEvent event) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void actionPerformed(ActionEvent event) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-	}
-	
 	public static void main(String[] args) {
+			
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				new Pong();
 			}
-		});
+		});}
+	
+	
+	
+	private void update() {
+		
+	}
+	
+	public void actionPerformed(ActionEvent event) {
+		update();
+		repaint();
 	}
 
 }
---
+++ Pong/src/PongPanel.java
@@ -0,0 +1,64 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Stroke;
+import java.awt.BasicStroke;
+
+public class PongPanel extends JPanel implements ActionListener, KeyListener {
+	private final static Color BACKGROUND_COLOUR = Color.BLACK;
+	private final static int TIMER_DELAY = 5;
+	
+	public PongPanel() {
+		setBackground(BACKGROUND_COLOUR);
+		Timer timer = new Timer(TIMER_DELAY, this);
+			timer.start();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+ 
+@Override
+public void paintComponent(Graphics g) {
+	super.paintComponent(g);
+	paintDottedLine(g);
+	
+}
+
+private void paintDottedLine(Graphics g) {
+	Graphics2D g2d = (Graphics2D) g.create();
+		Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] {9}, 0);
+		g2d.setStroke(dashed);
+		g2d.setPaint(Color.WHITE);
+		g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+		g2d.dispose();
+}
+
+}
